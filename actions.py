c308bce1b30d75e0c1129498b761b153f6d0569c
davidroma27 <davidroma27@gmail.com>
2023-04-15 15:19:23 +0200

    Added inactive_json register and refactorized code

--- actions.py
+++ actions.py
@@ -4,12 +4,13 @@ import tweepy.asynchronous
 from config import *
 from datetime import datetime, date, timedelta
 import random
-from main import getUserID
+import json
 
 # To set your environment variables in your terminal run the following line:
 os.environ['BEARER_TOKEN'] = BEARER_TOKEN
 bearer_token = os.environ.get('BEARER_TOKEN')
 
+# Get new access token the first time app executes
 if oauth2_access_token == "":
     print("(i) - Getting new access token...")
     get_access_token()
@@ -20,25 +21,88 @@ AsyncClient = tweepy.asynchronous.AsyncClient(bearer_token=bearer_token, consume
                                               access_token=access_token, access_token_secret=access_token_secret,
                                               return_type=dict, wait_on_rate_limit=True)
 oauth2AsyncClient = tweepy.asynchronous.AsyncClient(bearer_token=oauth2_access_token, consumer_key=consumer_key,
-                                              consumer_secret=consumer_secret,
-                                              access_token=access_token, access_token_secret=access_token_secret,
-                                              return_type=dict, wait_on_rate_limit=True)
+                                                    consumer_secret=consumer_secret,
+                                                    access_token=access_token, access_token_secret=access_token_secret,
+                                                    return_type=dict, wait_on_rate_limit=True)
+
+
+async def get_user_id(username) -> tuple:
+    """
+        Function to get User ID
+    :param username: The Twitter username
+    :return: user_id -> The ID of the user
+    """
+    global user_id
+    global errors
+    user_id = None
+    errors = None
+
+    user = await AsyncClient.get_user(username=username)
+    try:
+        if 'data' in user:
+            user_id = user['data']['id']
+            print(f"The {username}'s ID is : {user_id}")
+        elif 'errors' in user:
+            errors = user['errors']
+    except Exception as e:
+        print(f"Error: {e}")
+    return user_id, errors
+
+async def get_followed(user_id) -> tuple:
+    """
+    Returns a list of users the specified UserID is following
+    :param user_id: The Twitter user ID
+    :return: users - The list of followed users by the user
+    """
+    global users
+    global errors
+    users = []
+    errors = None
+
+    try:
+        list = await AsyncClient.get_users_following(id=user_id, max_results=1000)
+
+        # Check if list is returning data or returning errors
+        if "data" in list:
+            users = list['data']
+
+            # check if exists "next_token" and get next page of users
+            while 'next_token' in list['meta'].keys():
+                next_token = list['meta']['next_token']
+                list = await AsyncClient.get_users_following(id=user_id, max_results=1000, pagination_token=next_token)
+
+                for e in list['data']:
+                    users.append(e)
+
+                # double check for the last page
+                if 'next_token' in list['meta'].keys():
+                    next_token = list['meta']['next_token']  # get next token
+        elif 'errors' in list:
+            errors = list['errors'][0]
+    except Exception as e:
+        print(f"(!) ERROR: {e}")
+
+    return users, errors
 
 async def check_inactive(followed_users, period):
     """
-    Checks for each followed account if it is inactive depending on the period
+    Checks for each followed account if it is inactive depending on the period\n
     :param followed_users: The array of followed accounts
     :param period: maximum number of days of the last tweet
 
-    This function uses the Twitter API following endpoint:
+    This function uses the Twitter API following endpoint:\n
     - https://api.twitter.com/2/users/:id/tweets
-        > This endpoint is limited to 300 requests each 15 mins
+    > This endpoint is limited to 300 requests each 15 mins
     """
     period = int(period)
     inactives = []
     today = datetime.today()
     created_at = ""
     selected_users = []
+    inactive_json = {
+        "period": period,
+        "accounts": inactives
+    }
 
     # Calculates the limit date
     limit_date = today - timedelta(days=period)
@@ -51,17 +115,17 @@ async def check_inactive(followed_users, period):
 
         try:
             # Gets user ID for each account
-            userId = getUserID(username)[0]
+            user_id = await get_user_id(username)
 
             recent_tweets = []
             if not "data" in recent_tweets:
 
                 # Obtains the last 5 tweets from each followed account
-                recent_tweets = await AsyncClient.get_users_tweets(id=userId, max_results=5, tweet_fields="created_at",
-                                                       user_fields="username")
+                recent_tweets = await AsyncClient.get_users_tweets(id=user_id[0], max_results=5, tweet_fields="created_at",
+                                                                   user_fields="username")
                 # Skips private accounts
                 if "errors" in recent_tweets:
-                    if recent_tweets["errors"][0]["detail"] == f'Sorry, you are not authorized to see the user whith id: [{userId}].':
+                    if recent_tweets["errors"][0]["detail"] == f'Sorry, you are not authorized to see the user whith id: [{user_id[0]}].':
                         continue
 
                 # Obtains the date of the last tweet
@@ -71,17 +135,15 @@ async def check_inactive(followed_users, period):
                 else:
                     pass
 
-                # Checks if an account is inactive (tweet date is older than limit_date)
+                # Checks if an account is inactive (last posted tweet date is older than limit_date)
                 if created_at < limit_date:
-                    inactives.append(userId)
-
-        except tweepy.errors.TweepyException:
-            if tweepy.errors.TooManyRequests:
-                print("(!) - Twitter API rate limit has reached. Should wait 15 mins to rerun")
-                break
+                    inactives.append(int(user_id[0]))
+                    # Save period and inactive accounts into json
+                    with open("inactive_json.json", "w") as f:
+                        json.dump(inactive_json, f, indent=4)
         except Exception as e:
             print(f"(!) ERROR: {e}")
-            break
+            continue
 
     n_inactives = len(inactives)
     if n_inactives == 0:
@@ -95,36 +157,79 @@ async def check_inactive(followed_users, period):
             response = input().lower()
 
         if response == "y":
-            await unfollow_user(inactives)
+            inactive_json = {
+                "period": period,
+                "accounts": inactives
+            }
+            await unfollow_user(inactive_json)
         elif response == "n":
             print("(i) - Closing program...")
+            sys.exit(0)
 
-async def unfollow_user(inactives):
+
+async def unfollow_user(inactive_json):
     """
     Executes the unfollow action for each account in the array of inactive accounts
     :param inactives: The array of inactive accounts
 
-    This function uses the Twitter API unfollow endpoint:
+    This function uses the Twitter API unfollow endpoint:\n
     - https://api.twitter.com/2/users/:source_user_id/following/:target_user_id
-        > This endpoint is limited to 50 requests each 15 mins
+    > This endpoint is limited to 50 requests each 15 mins
     """
     unfollowed = []
 
-    for user in inactives:
+    for user in inactive_json["accounts"]:
         try:
-            await asyncio.shield(oauth2AsyncClient.unfollow_user(user))
+            await oauth2AsyncClient.unfollow_user(user)
             print(f"(i) - Unfollowing account with ID: {user}")
             unfollowed.append(user)
-        except tweepy.errors.TweepyException:
-            if tweepy.errors.TooManyRequests:
-                print("(!) - Twitter API rate limit has reached. Should wait 15 mins to rerun")
-                break
+            # Remove unfollowed user from json file
+            with open("inactive_json.json", "r") as output:
+                data = json.load(output)
+            data["accounts"].remove(user)
+            with open("inactive_json.json", "w") as output:
+                json.dump(data, output, indent=4)
         except Exception as e:
             print(f"(!) - ERROR: {e}")
-            break
+            continue
 
     n_unfollows = len(unfollowed)
     if n_unfollows == 0:
         print("(i) - No accounts have been unfollowed")
     else:
         print(f"(i) - {n_unfollows} inactive accounts have been unfollowed.")
+        sys.exit(0)
+
+
+async def check_json(period):
+    """
+    Check if exists a json file with inactive accounts.
+    If so, calls unfollow_user() with a list of that accounts
+    """
+    inactive_json = []
+    res = ""
+    try:
+        if os.path.exists("inactive_json.json") and os.path.getsize("inactive_json.json") > 0:
+            # Load the JSON data from the file into a Python dictionary
+            with open("inactive_json.json", "r") as f:
+                inactive_json = json.load(f)
+            # Checks if the introduced period is the same that the stored in json
+            if inactive_json["period"] == period:
+                # Checks if inactive list has accounts
+                if len(inactive_json["accounts"]) > 0:
+                    print("(i) - You haven't unfollow some inactive acocunts yet. Do you want to unfollow them? (y/n)")
+                    res = input().lower()
+
+                    while res not in ["y", "n"]:
+                        print("(!) - Invalid input. Valid answers are 'y' (yes) or 'n' (no)")
+                        res = input().lower()
+
+                    if res == "y":
+                        await unfollow_user(inactive_json)
+                    elif res == "n":
+                        pass
+            else:
+                pass
+    except Exception as e:
+        print(f"(!) - ERROR: {e}")
+        sys.exit(0)
--- main.py
+++ main.py
@@ -1,109 +1,28 @@
 import asyncio
-import os
-import json
-import tweepy
 from config import *
 import globals
 import actions
 
-# To set your environment variables in your terminal run the following line:
-os.environ['BEARER_TOKEN'] = BEARER_TOKEN
-bearer_token = os.environ.get('BEARER_TOKEN')
-
-# Configuring tweepy client
-client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
-                       access_token=access_token, access_token_secret=access_token_secret, return_type=dict)
-
-
-def getUserID(username) -> tuple:
-    """
-        Function to get User ID
-    :param username: The Twitter username
-    :return: user_id -> The ID of the user
-    """
-    global user_id
-    global errors
-    user_id = None
-    errors = None
-
-    user = client.get_user(username=username)
-    try:
-        if 'data' in user:
-            user_id = user['data']['id']
-            print(f"The {username}'s ID is : {user_id}")
-        elif 'errors' in user:
-            errors = user['errors']
-    except tweepy.errors.TweepyException:
-        if tweepy.errors.TooManyRequests:
-            print("(!) - Twitter API rate limit has reached. Should wait 15 mins to rerun")
-    except Exception as e:
-        print(f"Error: {e}")
-    return user_id, errors
-
-
-def getFollowed(user_id) -> tuple:
-    """
-    Returns a list of users the specified UserID is following
-    :param user_id: The Twitter user ID
-    :return: followed -> The list of followed users by the user
-    """
-    global users
-    global errors
-    users = []
-    errors = None
-
-    try:
-        list = client.get_users_following(id=user_id, max_results=1000)
-
-        # Check if list is returning data or returning errors
-        if "data" in list:
-            users = list['data']
-            with open("followed.json", "w") as output:
-                output.write(json.dumps(users))
-
-            # check if exists "next_token" and get next page of users
-            while 'next_token' in list['meta'].keys():
-                next_token = list['meta']['next_token']
-                list = client.get_users_following(id=user_id, max_results=1000, pagination_token=next_token)
-
-                for e in list['data']:
-                    users.append(e)
-
-                with open("followed.json", "w") as output:
-                    output.write(json.dumps(users))
-
-                # double check for the last page
-                if 'next_token' in list['meta'].keys():
-                    next_token = list['meta']['next_token']  # get next token
-        elif 'errors' in list:
-            errors = list['errors'][0]
-    except tweepy.errors.TweepyException:
-        if tweepy.errors.TooManyRequests:
-            print("(!) - Twitter API rate limit has reached. Should wait 15 mins to rerun")
-    except Exception as e:
-        print(f"(!) ERROR: {e}")
-
-    return users, errors
-
 
 async def main():
     """
-        MAIN flow of the app.
-        Asks for a valid username in a infinite loop.
-        Once a username is entered calls the following functions:
-        1- getUserID
+        MAIN flow of the app.\n
+        1. Asks for a valid username in an infinite loop.\n
+        2. Asks for a period to check if accounts are inactive
+        Once a username is entered calls the following functions:\n
+        -get_user_id()
             > If username not exists, app will ask again for a username
-        2- getFollowed
-            > If user entered a private account, app will ask again for a non private account
+        -get_followed()
+            > If user entered a private account, app will ask again for a non-private account
     """
+
+    global username
+    global period
 
     print("<> Welcome to MacUnfollows <>")
     print("> To begin enter a valid Twitter username:")
 
     # loop for control inputs flows
-    global username
-    global period
-
     username = input()
     while len(username) == 0:
         print("(!) - Username cannot be empty")
@@ -116,10 +35,12 @@ async def main():
         period = input()
     else:
         try:
-            user_id = getUserID(username)
+            # Firstly check if there are pending accounts to unfollow
+            await actions.check_json(int(period))
+            user_id = await actions.get_user_id(username)
             if user_id[0] is not None:
                 print("(i) - Getting followed accounts...")
-                globals.followed_users = getFollowed(user_id[0])
+                globals.followed_users = await actions.get_followed(user_id[0])
                 n_followed = len(globals.followed_users[0])
                 print(f"(i) - Found {n_followed} followed accounts")
                 if globals.followed_users[0] is not None:
@@ -127,13 +48,8 @@ async def main():
                         print("(!) - This user does not follow any account")
                         print("> Please enter a valid username:")
                     else:
-                        # print(username)
-                        # print(period)
-                        try:
-                            print("(i) - Looking for inactive accounts...")
-                            await asyncio.shield(actions.check_inactive(globals.followed_users[0], period))
-                        except asyncio.TimeoutError:
-                            print("(!) - Timeout async error")
+                        print("(i) - Looking for inactive accounts...")
+                        await asyncio.shield(actions.check_inactive(globals.followed_users[0], period))
                 elif globals.followed_users[1] is not None:
                     error = globals.followed_users[1]
                     if error[0]["title"] == 'Authorization Error':
@@ -144,9 +60,8 @@ async def main():
                 if error[0]["title"] == 'Not Found Error':
                     print("(i) - The username entered does not exist")
                     print("> Please enter a valid username:")
-        except tweepy.errors.TweepyException:
-            if tweepy.errors.TooManyRequests:
-                print("(!) - Twitter API rate limit has reached. Should wait 15 mins to rerun")
+        except asyncio.TimeoutError:
+            print("(!) - Timeout async error")
         except Exception as e:
             print(f"(!) ERROR: {e}")
 
@@ -156,6 +71,6 @@ if __name__ == "__main__":
     asyncio.run(main())
 
     # getFollowed(1110821672507056128)
-    # getUserID("macncheesys")
+    # get_user_id("macncheesys")
     # asyncio.run(checkAccounts.check_inactive([], 365))
     # asyncio.run(actions.unfollow_user([956585870693453824]))
